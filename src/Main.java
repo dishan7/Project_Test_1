6a02a108bba95abca175e3c54e11e20ac328bcc0
Dishan Chutani <chutanidishan@gmail.com>
2026-07-22 02:27:31 +0530

    more changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        int sum = numbers.stream()
+                .filter(n -> n%2 == 0)
+                .mapToInt(Integer::intValue).sum();
+        System.out.println("Sum of even numbers: " + sum);
     }
 }
